eb26f179833fbad1630b1ea1552a1f43ef0dfeb5
Firouzfar <firouzfar.sebastien@gmail.com>
2021-11-19 15:03:24 +0100

    Create a route file / creation d'un dossier route

--- vue-mevn-crud/backend/routes/student.route.js
+++ vue-mevn-crud/backend/routes/student.route.js
@@ -5,6 +5,59 @@ const studentRoute = express.Router()
 const StudentModel = require("../models/Student")
 
 //Get All data
-studentRoute.get('/', (req, res) => {
+studentRoute.get('/', (req, res, next) => {
+    StudentModel.find((error, data) => {
+        if(error){
+            return next(error); 
+        }else{
+            res.json(data)
+        }
+    })
+})
 
+//create student data
+studentRoute.post('/create-student', (req, res, next) => {
+    StudentModel.create((req.body, (error, data) => {
+        if(error){
+            return next(error); 
+        }else{
+            res.json(data)
+        }
+    }))
+})
+
+studentRoute.get('/edit-student/:id', (req, res, next) => {
+    StudentModel.findById(req.params.id, (error, data) => {
+        if(error){
+            return next(error); 
+        }else{
+            res.json(data)
+        }
+    })
+})
+
+studentRoute.put('/update-student/:id', (req, res,next)=>{
+    StudentModel.findByIdAndUpdate(req.params.id, {
+        $set : req.body
+    }, (error, data)=>{
+        if(error){
+            return next(error); 
+        }else{
+            res.json(data)
+            console.log('Student successfully updated')
+        }
+    })
+})
+
+//Delete student data
+studentRoute.delete('/delete-student/:id', (req, res, next)=>{
+    StudentModel.findByIdAndDelete(req.params.id,(error, data)=>{
+        if(error){
+            return next(error); 
+        }else{
+            res.status(200).json({
+                msg : data
+            })
+        }
+    })
 })
